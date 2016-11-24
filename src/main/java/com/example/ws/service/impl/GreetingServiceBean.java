c2ec2ed6e4af00dbb0d5c2a9ee3ad4fb717ec0a5
Rolex Brown <ranjanghimire@gmail.com>
2016-11-24 17:28:23 -0600

    Implemented Repository layer for the application with schema, data and application properties

--- src/main/java/com/example/ws/service/impl/GreetingServiceBean.java
+++ src/main/java/com/example/ws/service/impl/GreetingServiceBean.java
@@ -4,94 +4,59 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.ws.model.Greeting;
+import com.example.ws.repository.GreetingRepository;
 import com.example.ws.service.GreetingService;
 
 @Service
 public class GreetingServiceBean implements GreetingService {
 
-	//Declare id and Map to store greetings
-		private static Long nextId;
-		private static Map<Long,Greeting> greetingMap;
-		
-		//This is static method to save greetings
-		private static Greeting save(Greeting greeting){
-			if (greetingMap == null){
-				greetingMap = new HashMap<Long,Greeting>();
-				nextId = 1L;
-			}
-			
-			//If update ..
-			if (greeting.getId() != null){ 
-				Greeting oldGreeting = greetingMap.get(greeting.getId());
-				if (oldGreeting == null){
-					return null; //if the particular greeing is not present, no update.
-				}
-				//remove the old greeting and put the new greeting. (update simulation)
-				greetingMap.remove(greeting.getId());
-				greetingMap.put(greeting.getId(), greeting);
-				return greeting;
-			}
-			
-			//If create.. 
-			greeting.setId(nextId);
-			nextId += 1;
-			greetingMap.put(greeting.getId(), greeting);
-			return greeting;
-		}
-		
-		//delete helper method
-		private static boolean remove(Long id){
-			Greeting deletedGreeting = greetingMap.remove(id);
-			if (deletedGreeting == null){
-				return false;
-			}
-			return true;
-		}
-		
-		static{
-			Greeting g1 = new Greeting();
-			g1.setText("Hello World");
-			save(g1);
-			
-			Greeting g2 = new Greeting();
-			g2.setText("Namaskar Hajur!");
-			save(g2);
-		}
-	
+	@Autowired
+	private GreetingRepository greetingRepository;
+
 	public GreetingServiceBean() {
 		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public Collection<Greeting> findAll() {
-		Collection<Greeting> greetings = greetingMap.values();
+		Collection<Greeting> greetings = greetingRepository.findAll();
 		return greetings;
 	}
 
 	@Override
 	public Greeting findOne(Long id) {
-		Greeting greeting = greetingMap.get(id);
+		Greeting greeting = greetingRepository.findOne(id);
 		return greeting;
 	}
 
 	@Override
 	public Greeting create(Greeting greeting) {
-		Greeting savedGreeting = save(greeting);
+		if (greeting.getId() != null) {
+			// cannot create Greeting with specified id value
+			return null;
+		}
+		Greeting savedGreeting = greetingRepository.save(greeting);
 		return savedGreeting;
 	}
 
 	@Override
 	public Greeting update(Greeting greeting) {
-		Greeting updateGreeting = save(greeting);
+		Greeting greetingPersisted = findOne(greeting.getId());
+		if (greetingPersisted == null) {
+			// cannot update greeting that hasn't been persisted
+			return null;
+		}
+		Greeting updateGreeting = greetingRepository.save(greeting);
 		return updateGreeting;
 	}
 
 	@Override
 	public void delete(Long id) {
-		remove(id);
+		greetingRepository.delete(id);
 	}
 
 }

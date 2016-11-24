dad9499ad394e6aec3ca75cf6d1061823a3413d9
Rolex Brown <ranjanghimire@gmail.com>
2016-11-24 16:53:48 -0600

    Added jpa dependencies to pom and annotated model class as entity

--- src/main/java/com/example/ws/model/Greeting.java
+++ src/main/java/com/example/ws/model/Greeting.java
@@ -1,6 +1,13 @@
 package com.example.ws.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Greeting {
+	@Id
+	@GeneratedValue
 	private Long id;
 	private String text;
 
